b2d48ad83f42965c75c5b315a45205127185ff83
oliverloza <oliverbree@gmail.com>
2023-08-07 15:06:21 -0700

    completed array filter method

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, query) {
+const lowerQuery = query.toLowerCase();
+const matchingDrivers = drivers.filter(driver =>
+    driver.toLowerCase().includes(lowerQuery));
+    return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, query) {
+const lowerQuery = query.toLowerCase();
+const matchingDrivers = drivers.filter(driver =>
+    driver.toLowerCase().startsWith(lowerQuery));
+    return matchingDrivers;
+}
+
+function matchName(drivers, name) {
+const matchingDrivers = drivers.filter(driver =>
+    driver.name === name);
+    return matchingDrivers;
+}
